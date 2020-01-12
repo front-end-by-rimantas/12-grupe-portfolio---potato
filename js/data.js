a8edaf14dc6fc9f9a7c3b023ec4097c31f72ccc1
Gabrieliusv <gabrielius.svi@gmail.com>
2020-01-12 17:21:21 +0200

    Services

--- js/data.js
+++ js/data.js
@@ -1,6 +1,43 @@
 "use strict";
 // Services
-
+const services = [
+  {
+    icon: "object-ungroup",
+    title: "Graphic Design",
+    description:
+      "Fames urna class volutpat malesuad cubilia ornare maecenas fermentum conub gravida purus."
+  },
+  {
+    icon: "object-ungroup",
+    title: "3D Motion",
+    description:
+      "Fames urna class volutpat malesuad cubilia ornare maecenas fermentum conub gravida purus."
+  },
+  {
+    icon: "object-ungroup",
+    title: "Web Design",
+    description:
+      "Fames urna class volutpat malesuad cubilia ornare maecenas fermentum conub gravida purus."
+  },
+  {
+    icon: "object-ungroup",
+    title: "Web Development",
+    description:
+      "Fames urna class volutpat malesuad cubilia ornare maecenas fermentum conub gravida purus."
+  },
+  {
+    icon: "object-ungroup",
+    title: "Photography",
+    description:
+      "Fames urna class volutpat malesuad cubilia ornare maecenas fermentum conub gravida purus."
+  },
+  {
+    icon: "object-ungroup",
+    title: "Video Editing",
+    description:
+      "Fames urna class volutpat malesuad cubilia ornare maecenas fermentum conub gravida purus."
+  }
+];
 // Skills
 
 //Portfolio
--- js/functions.js
+++ js/functions.js
@@ -1,2 +1,48 @@
 "use strict";
 
+function renderServices(target, data) {
+  let HTML = "";
+
+  const targetDOM = document.querySelector(target);
+
+  if (!Array.isArray(data)) {
+    return console.error("ERROR: Incorrect services data format.");
+  }
+  if (data.length === 0) {
+    return console.error("ERROR: Services data array is empty");
+  }
+
+  for (let i = 0; i < data.length; i++) {
+    const service = data[i];
+    HTML += `<div class="service-item col-4 col-md-6 col-sm-12 mb-30">
+        <div class="block">
+          <div class="icon-container">
+            <div class="circle">
+              <div class="dot"></div>
+            </div>
+            <i class="fa fa-${service.icon}"></i>
+          </div>
+          <h4 class="mv-20">${service.title}</h4>
+          <p class="color-secondary">${service.description}</p>
+        </div>
+      </div>`;
+  }
+
+  targetDOM.innerHTML = HTML;
+
+  const serviceItems = targetDOM.querySelectorAll(".service-item");
+
+  const iconCircles = targetDOM.querySelectorAll(".circle");
+
+  for (let i = 0; i < serviceItems.length; i++) {
+    serviceItems[i].addEventListener("mouseover", e => {
+      iconCircles[i].classList.add("play");
+    });
+
+    serviceItems[i].addEventListener("mouseout", e => {
+      iconCircles[i].classList.remove("play");
+    });
+  }
+
+  return;
+}
